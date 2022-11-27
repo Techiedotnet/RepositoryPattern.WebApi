d0de40873477b693a20dcfbe7dab8148cec92bb0
Techie Developer <harinathpinna@gmail.com>
2022-11-27 22:12:59 +0530

    Add project files.

---
+++ DataAccess.EFCore/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.EFCore
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Developer> Developers { get; set; }
+    }
+}
---
+++ Domain/Interfaces/IDeveloperRepository.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities;
+using System.Collections.Generic;
+
+namespace Domain.Interfaces
+{
+    public interface IDeveloperRepository : IGenericRepository<Developer>
+    {
+        IEnumerable<Developer> GetPopularDevelopers(int count);
+    }
+}
